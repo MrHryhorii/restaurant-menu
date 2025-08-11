8e85658a87bfaf9fccfa00557545bbccaecc7e23
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-11 14:01:25 +0200

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://mrhryhorii.github.io/restaurant-menu/",
   plugins: [react()],
 })
